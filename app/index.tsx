9517436eb4d91d459c0e564f65878182c26aec81
Wizxrdx <benedick.dumol@gmail.com>
2024-10-12 00:20:22 +0800

    ILO1 and ILO2

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
-import { StyleSheet, View, Text, TextInput, Button } from "react-native";
 import { useState } from "react";
+import { StyleSheet, View, Text, TextInput, Button, Image } from "react-native";
 
 export default function Index() {
+  
   const [enteredGoalText, setEnteredGoalText] = useState("");
   const [courseGoals, setCourseGoals] = useState<string[]>([]);
 
@@ -24,10 +25,12 @@ export default function Index() {
           style={ styles.textInput }
           onChangeText={ goalInputHandler }
         ></TextInput>
-        <Button title="Add Goal" onPress={ addGoalHandler }></Button>
+        <Button title="Add Goal" onPress={ addGoalHandler } color={'#4CAF50'}></Button>
       </View>
       <View style={ styles.goalsContainer }>
-          { courseGoals.map((goal: string) => <Text key={goal}>{goal}</Text>) }
+          { courseGoals.map((goal: string) => <Text style={ styles.goalsComponent } key={goal}>
+            <Image source={{ uri: "https://cdn-icons-png.flaticon.com/512/1657/1657671.png" }} style={{ width: 20, height: 20, marginRight: 5 }}/>
+            {goal}</Text>) }
       </View>
     </View>
   );
@@ -35,26 +38,38 @@ export default function Index() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
+    backgroundColor: '#f7f7f7',
     paddingHorizontal: 16,
-    flex: 1
+    paddingVertical: 24
   },
   inputContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderColor: "#cccccc",
-    flex: 1
+    marginBottom: 6,
+    backgroundColor: '#fff',
+    padding: 16,
+    borderRadius: 10,
+    elevation: 5
   },
   goalsContainer: {
-    flex: 5
+    flex: 5,
+    padding: 6
   },
   textInput: {
-    borderWidth: 2,
-    borderColor: "#cccccc",
+    borderWidth: 1,
+    borderColor: "#ddd",
     width: "70%",
     marginRight: 8,
-    padding: 13
+    padding: 13,
+    borderRadius: 5
+  },
+  goalsComponent: {
+    padding: 16,
+    marginBottom: 8,
+    borderRadius: 5,
+    backgroundColor: '#4CAF50',
+    elevation: 5
   }
 });
