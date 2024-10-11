af302a3c8ab3aa350cacd8468b1d3b8d331acb1f
Wizxrdx <benedick.dumol@gmail.com>
2024-10-11 23:24:01 +0800

    lab 7.1

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,60 @@
-import { Text, View } from "react-native";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+import { useState } from "react";
 
 export default function Index() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [courseGoals, setCourseGoals] = useState<string[]>([]);
+
+  function goalInputHandler(enteredText : string) {
+    setEnteredGoalText(enteredText);
+  };
+
+  function addGoalHandler() {
+    setCourseGoals((currentCourseGoals) =>[
+      ...currentCourseGoals,
+      enteredGoalText,
+    ]);
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={ styles.appContainer }>
+      <View style={ styles.inputContainer }>
+        <TextInput
+          placeholder="Your Course Goal"
+          style={ styles.textInput }
+          onChangeText={ goalInputHandler }
+        ></TextInput>
+        <Button title="Add Goal" onPress={ addGoalHandler }></Button>
+      </View>
+      <View style={ styles.goalsContainer }>
+          { courseGoals.map((goal: string) => <Text key={goal}>{goal}</Text>) }
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  appContainer: {
+    paddingHorizontal: 16,
+    flex: 1
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderColor: "#cccccc",
+    flex: 1
+  },
+  goalsContainer: {
+    flex: 5
+  },
+  textInput: {
+    borderWidth: 2,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 8,
+    padding: 13
+  }
+});
