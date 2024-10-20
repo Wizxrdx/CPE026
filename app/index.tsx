4b1387fdc99a17ec5da9d25badba5edfbc9a6735
Wizxrdx <benedick.dumol@gmail.com>
2024-10-20 09:16:48 +0800

    fixed: unmatch route issue

--- app/index.tsx
+++ app/index.tsx
@@ -29,17 +29,18 @@ export default function App() {
       <GoalInput onAddGoal={addGoalHandler}/>
       <Text style={ styles.goalsHeader }>List of Goals</Text>
 
-      <View style={ styles.goalsContainer }>
+      {/* <View style={ styles.goalsContainer }>
         <CustomGoalItemUsingScrollView goalsData={ generateGoals().map(goal => goal.text) }/>
       </View>
       <View style={ styles.goalsContainer }>
       <FlatList style={ styles.goalsContainer } data={ generateGoals() } renderItem={(item) => {
         return <CustomGoalItemUsingFlatList text={item.item.text}/>
         }}/>
-        </View>
-      {/* <FlatList data={ courseGoals } renderItem={(item) => {
+        </View> */}
+        
+      <FlatList style={ styles.goalsContainer } data={ courseGoals } renderItem={(item) => {
         return <GoalItem text={item.item.text}/>
-      }}/> */}
+      }}/>
     </View>
   );
 }
